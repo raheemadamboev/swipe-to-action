298f5e0c1996e1041c07dac76e4413159c28cc65
raheemadamboev <vividricemaryandmax@gmail.com>
2024-03-05 22:43:58 +0500

    Installed dependencies;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,18 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.android)
+    alias(libs.plugins.kotlin)
 }
 
 android {
     namespace = "xyz.teamgravity.swipetoaction"
-    compileSdk = 34
+    compileSdk = libs.versions.sdk.compile.get().toInt()
 
     defaultConfig {
         applicationId = "xyz.teamgravity.swipetoaction"
-        minSdk = 26
-        targetSdk = 34
+        minSdk = libs.versions.sdk.min.get().toInt()
+        targetSdk = libs.versions.sdk.target.get().toInt()
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -23,23 +22,38 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = libs.versions.java.target.get()
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 }
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    // material
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
+    // constraintlayout
+    implementation(libs.constraintlayout)
+
+    // recyclerview
+    implementation(libs.recyclerview)
+
+    // core
+    implementation(libs.core)
+
+    // appcompat
+    implementation(libs.appcompat)
+
+    // activity
+    implementation(libs.activity)
 }
